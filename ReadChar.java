d224403134318c347d0df95a082ebabdb0443ae1
John Gilbert K T <johngilbert.kochery@gmail.com>
2023-01-31 00:12:53 +0530

    just modification

--- ReadChar.java
+++ ReadChar.java
@@ -1,16 +1,19 @@
 //reading charachor;
 import java.io.*;
+import java.util.*;
 public class ReadChar{
 	public static void main(String args[]){
 		char c;
 		DataInputStream input = new DataInputStream(System.in);
-		
+		Scanner sc = new Scanner(System.in);
 		
 		try
 		{
 		System.out.println("Enter charachor :");
 		c=(char)input.read();
+		char ch = sc.nextLine().charAt(0);// this will get the char from specific index
 		System.out.println("charachor is "+c);
+		System.out.println("charachor is "+ch);
 		
 		}
 		catch(IOException e){
