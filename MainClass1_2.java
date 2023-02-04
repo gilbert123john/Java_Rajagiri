cfb46d7522d3b4eda47f23f8e2a7810d30231346
John Gilbert K T <johngilbert.kochery@gmail.com>
2023-02-04 11:30:51 +0530

    updated with lab cycle 1

---
+++ MainClass1_2.java
@@ -0,0 +1,10 @@
+public class MainClass1_2
+{
+    public static void main(String args[]) {
+        int x,y,p;
+        x=Integer.parseInt(args[0]);
+        y=Integer.parseInt(args[1]);
+        p=x*y;
+        System.out.println("Product of "+x+" and "+y+" = "+p);
+    }
+}
